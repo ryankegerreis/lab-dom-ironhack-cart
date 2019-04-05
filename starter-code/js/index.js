8efa8a8af295ae44ce73c25e1355798b6147646f
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-04-05 13:05:07 -0400

    Button working

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,4 +1,6 @@
+
 function deleteItem(e){
+
 
 }
 
@@ -10,10 +12,24 @@ function updatePriceByProduct(productPrice, index){
 
 }
 
-function getTotalPrice() {
+function getTotalPrice(price) {
+// Button clicks and returns a value.
+
+// NEXTSTEPS: Create output area for price in
+let total = 0
+
+  document.getElementById("calculate-price-button").onclick = console.log("Button works");
+
+  let valueOutput = document.getElementsByClassName("input")[0]
+
+  let quantity = valueOutput.value
+
+console.log(quantity * 5)
+
+total = price * quantity;
 
 }
-
+ 
 function createQuantityInput(){
 
 }
@@ -39,14 +55,34 @@ function createNewItem(){
 }
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
+
+  var calculatePriceButton = document.getElementById('calculate-price-button');
+
+  console.log(calculatePriceButton)
   var createItemButton = document.getElementById('new-item-create');
+
+  console.log(createItemButton)
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
+  console.log(deleteButtons)
   calculatePriceButton.onclick = getTotalPrice;
+
+
   createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
+
+//=============== Button Click Event ===================
+
+// var counter {
+//   // manipulate the innerHTML of the element targeted
+//   theCounter += 1;
+//   console.log("I Clicked the Button!!!!!");
+
+//   // return the newly updated number to the original element
+//   counter.innerHTML = theCounter;
+// }
